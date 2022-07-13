3cb7df87dba40ceb78e94046430e51efad4c66eb
necaprogramer <nissimbara@outlook.com>
2022-07-13 19:28:38 +0200

    Add CSS style to elements and function needed for etch a sketch to function

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let container = document.getElementById('container-for-etchasketch');
+
+let buttonForNumberOfSquares = document.getElementById('number-of-squares');
+
+creatingSquares(16);
+
+buttonForNumberOfSquares.addEventListener('click', (number) => {
+    number = prompt(`Enter the number of squares: `);
+    if(number > 100){
+        alert(`Maximum number of squares is 100!`);
+    }else{
+
+    }
+});
+
+function creatingSquares(number){
+    for(let i = 0; i < number * number; i++){
+        let square = document.createElement('div');
+        container.appendChild(square);
+        square.classList.add('square');
+        coloringSquares(square);
+    };
+};
+
+function coloringSquares(squares){
+    squares.addEventListener('mouseenter', () => {
+        squares.style.backgroundColor = 'black';
+    });
+};
+
+function randomisingColors(){
+    let R = Math.floor(Math.random() * 255),
+        G = Math.floor(Math.random() * 255),
+        B = Math.floor(Math.random() * 255);
+    return `rgb(${R}, ${G}, ${B})`;
+};
